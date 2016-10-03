dad9a43c78da0e31f343dabbbe17a13d40229d11
Adrian Babilinski <ababilinski@gmail.com>
2016-10-03 16:00:14 -0400

    added spherecast to get items using overlap sphere

---
+++ Assets/VRInput/Scripts/VRInteractions.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+namespace BabilinApps.VRInput {
+    public class VRInteractions : MonoBehaviour {
+        
+        /// <summary>
+        /// Allows user to cast a sphere and get the closest item to the location given.
+        /// </summary>
+        /// <param name="location"> Center of the sphere cast</param>
+        /// <param name="radius"> radius of the sphere cast</param>
+        /// <param name="hit"> the closest item returned</param>
+        public static bool SphereCast(Vector3 location, float radius, out VRInteractions hit) {
+            hit = null;
+            var castObjects = Physics.OverlapSphere(location, radius);
+            List<VRInteractions> hitItems = new List<VRInteractions>();
+            foreach (var item in castObjects) {
+                var interactionObject = item.GetComponent<VRInteractions>();
+                if (interactionObject != null)
+                    hitItems.Add(interactionObject);
+            }
+            hitItems.Sort((a, b) => Vector3.Distance(a.transform.position, b.transform.position).CompareTo(Vector3.Distance(a.transform.position, b.transform.position)));
+
+            if (hitItems.Count > 0) {
+                hit = hitItems[0];
+                return true;
+            }
+            else {
+                return false;
+            }
+
+        }
+    }
+}
